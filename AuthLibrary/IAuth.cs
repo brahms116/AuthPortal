bdf37a66f9a1d0eafed1f2da5c61bfd6fa1203ec
David Kwong <20544dk@gmail.com>
2022-08-31 11:40:41 +1000

    Cognito User Repo

---
+++ AuthLibrary/IAuth.cs
@@ -0,0 +1,15 @@
+﻿namespace AuthLibrary;
+
+public interface IAuth
+{
+    public Task<CreateUserResponse> CreateUser(string email, string username);
+
+    public Task<LoginUserResponse> Login(string username, string password);
+
+    public Task ChangePassword(string username, string oldPassword, string newPassword);
+
+    public Task ForgotPassword(string username);
+
+    public Task ConfirmForgotPassword(string username, string newPassword, string confirmationCode);
+
+}
--- AuthPortalDemoClient/Program.cs
+++ AuthPortalDemoClient/Program.cs
@@ -1,18 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Amazon.CognitoIdentityProvider;
-using AwsAuthLibrary;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AuthLibrary;
 
-using IHost host = Host.CreateDefaultBuilder(args).Build();
+using IHost host = Host.CreateDefaultBuilder(args).UseEnvironment("Development").Build();
 
 IConfiguration config = host.Services.GetRequiredService<IConfiguration>();
 
 var provider = new AmazonCognitoIdentityProviderClient();
 
 
-var repo = new UserRepository(new UserRepositoryConfig()
+var repo = new CognitoUserRepository(new CognitoUserRepositoryConfig()
 {
     Provider = provider,
     UserpoolId = config.GetValue<string>("USERPOOL_ID"),
@@ -22,10 +22,14 @@ var repo = new UserRepository(new UserRepositoryConfig()
 
 
 //var result = await repo.CreateUser("20544dk@gmail.com", "20544");
-var result = await repo.Login("20544", "Password@121251252");
+//var result = await repo.Login("20544", "Password@1234");
 //await repo.ChangePassword("20544", ";d3JaDNC", "");
 
-Console.WriteLine(result.Token.ToString());
+//await repo.ForgotPassword("20544");
+
+await repo.ConfirmForgotPassword("20544", "Password@12345", "128611");
+
+//Console.WriteLine(result.Token.ToString());
 Console.ReadLine();
 
 
